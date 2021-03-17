603f2452a1a886e5931c0f5553662c3b84b442d3
Abhishek Muvvala <abhishekmuvvala@Abhisheks-MacBook-Air.local>
2021-03-17 17:52:33 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div id="root">
+      <h1 data-ns-test= "project-name">
+      project-name
+      </h1>
+      <p data-ns-test= "project-name">project description</p>
+    </div>
+  )
+}
+
+
+export default App;
